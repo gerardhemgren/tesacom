4b17bb280b3aa5a5b389c194888fe23ffbd95531
gerard hemgren <gerardhemgren@gmail.com>
2022-10-04 19:09:30 -0300

    add float type

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const avro = require('avsc');
 const pkg = require('./input/data.json')
 
 const schema = fs.readFileSync(path.join(__dirname, 'schema', 'Package.avsc'), 'utf-8');
-const type = avro.Type.forSchema(JSON.parse(schema));
+const type = avro.parse(schema);
 
 const encoded = type.toBuffer(pkg);
 const decoded = type.fromBuffer(encoded);
 
 console.log(encoded.toString('hex'));
-console.log(Buffer.byteLength(encoded));
 console.log(decoded);
+
+console.log('JSON size', Buffer.byteLength(JSON.stringify(pkg)));
+console.log('Avro size', Buffer.byteLength(encoded));
