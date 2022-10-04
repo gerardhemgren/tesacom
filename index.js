ae83ff504d4dc86002e839928f0282f5feaf4bfe
gerard hemgren <gerardhemgren@gmail.com>
2022-10-04 18:18:09 -0300

    use avro

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello')
+const fs = require('fs');
+const path = require('path');
+const avro = require('avsc');
+const pkg = require('./input/data.json')
+
+const schema = fs.readFileSync(path.join(__dirname, 'schema', 'Package.avsc'), 'utf-8');
+const type = avro.Type.forSchema(JSON.parse(schema));
+
+const encoded = type.toBuffer(pkg);
+const decoded = type.fromBuffer(encoded);
+
+console.log(encoded.toString('hex'));
+console.log(Buffer.byteLength(encoded));
+console.log(decoded);
