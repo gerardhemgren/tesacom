bde1898cf40f3da03e0f1719a5f0b9d633301f8d
gerard hemgren <gerardhemgren@gmail.com>
2022-10-04 22:48:16 -0300

    add try/catch and provisional example 2

--- index.js
+++ index.js
@@ -3,14 +3,42 @@ const path = require('path');
 const avro = require('avsc');
 const pkg = require('./input/data.json')
 
-const schema = fs.readFileSync(path.join(__dirname, 'schema', 'Package.avsc'), 'utf-8');
-const type = avro.parse(schema);
+// Ejemplo 1
 
-const encoded = type.toBuffer(pkg);
-const decoded = type.fromBuffer(encoded);
+function exampleOne() {
+    try {
+        const schema = fs.readFileSync(path.join(__dirname, 'schema', 'Package.avsc'), 'utf-8');
+        const type = avro.parse(schema);
+        const encoded = type.toBuffer(pkg);
+        const decoded = type.fromBuffer(encoded);
+        const re = () => {
+            console.log(encoded.toString('hex'));
+            console.log(decoded);
+            console.log('JSON size', Buffer.byteLength(JSON.stringify(pkg)));
+            console.log('Avro size', Buffer.byteLength(encoded));
+            console.log('________')
+        };
+        return re();
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
+}
 
-console.log(encoded.toString('hex'));
-console.log(decoded);
+// Otro Ejemplo de deserialización
 
-console.log('JSON size', Buffer.byteLength(JSON.stringify(pkg)));
-console.log('Avro size', Buffer.byteLength(encoded));
+function exampleTwo() {
+    try {
+        const schema = fs.readFileSync(path.join(__dirname, 'schema', 'PackageTwo.avsc'), 'utf-8');
+        const type = avro.parse(schema);
+        const trama = Buffer.from([1, 2, 3]); 
+        const decoded = type.fromBuffer(trama);
+        return console.log(decoded);
+    } catch (e) {
+        console.log('can not decode', e);
+        return false;
+    }
+}
+
+exampleOne();
+exampleTwo();
